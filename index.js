b58a1e4d86303980c4970bc7cf9554ca107ea714
Juan Esteban Arango Zapata <juaneste687@gmail.com>
2021-05-25 17:09:30 -0500

    Ciclos

--- index.js
+++ index.js
@@ -1,80 +1,75 @@
-//Estructuras de flujo
+//Ciclos
 
-//Condicionales
+//LOS FOR
 
-//IF
-let edad = 10
-if( edad >= 18 ){
-    console.log(`Ya tienes ${edad}, claro que puedes pasar`)
+//FOR BASICO
+
+for(let contador = 0; contador <= 10; contador++ ){
+    console.log( contador )
 }
-else if( isNaN( edad ) ){
-    console.log(`Debes ingresar un numero >:(`)
-}else{
-    console.log(`Apenas tienes ${edad}, claro que NO puedes pasar`)
+//FOR IN
+let arreglo = [
+    {
+      "id": 7,
+      "email": "michael.lawson@reqres.in",
+      "first_name": "Michael",
+      "last_name": "Lawson",
+      "avatar": "https://reqres.in/img/faces/7-image.jpg"
+    },
+    {
+      "id": 8,
+      "email": "lindsay.ferguson@reqres.in",
+      "first_name": "Lindsay",
+      "last_name": "Ferguson",
+      "avatar": "https://reqres.in/img/faces/8-image.jpg"
+    },
+    {
+      "id": 9,
+      "email": "tobias.funke@reqres.in",
+      "first_name": "Tobias",
+      "last_name": "Funke",
+      "avatar": "https://reqres.in/img/faces/9-image.jpg"
+    },
+    {
+      "id": 10,
+      "email": "byron.fields@reqres.in",
+      "first_name": "Byron",
+      "last_name": "Fields",
+      "avatar": "https://reqres.in/img/faces/10-image.jpg"
+    },
+    {
+      "id": 11,
+      "email": "george.edwards@reqres.in",
+      "first_name": "George",
+      "last_name": "Edwards",
+      "avatar": "https://reqres.in/img/faces/11-image.jpg"
+    },
+    {
+      "id": 12,
+      "email": "rachel.howell@reqres.in",
+      "first_name": "Rachel",
+      "last_name": "Howell",
+      "avatar": "https://reqres.in/img/faces/12-image.jpg"
+    }
+  ]
+
+  for( let indice in arreglo ){
+      console.log( arreglo[indice].first_name )
+  }
+
+  //FOR OF
+  for( let elemento of arreglo ){
+      console.log( elemento.first_name )
+  }
+
+//LOS WHILE
+
+//WHILE NORMAL
+let i = 0
+while(true){
+    console.log("SAMUEL")
+    if( i == 6 ){
+        break
+    }
+    i ++
 }
-//OPERADOR TERNARIO
-let puedePasar = edad >= 18 ? `Ya tienes ${edad}, claro que puedes pasar` : `Apenas tienes ${edad}, claro que NO puedes pasar`
-//SWITCH
-let dia = 1
-let diatexto
-switch( dia ){
-    case 1:
-        diatexto = "Lunes"
-        break
-    case 2: 
-        diatexto = "Martes"
-        break
-    case 3: 
-        diatexto = "Miercoles"
-        break
-    case 4: 
-        diatexto = "Jueves"
-        break
-    case 5: 
-        diatexto = "Viernes"
-        break
-    case 6: 
-        diatexto = "Sabado"
-        break
-    case 7: 
-        diatexto = "Domingo"
-        break
-    default:
-        diatexto = "DESCONOCIDO"
-}
-console.log( "Feliz " + diatexto )
-
-
-/*
-Operadores relacionales:
-
-== Igual normal 5 == "5" true
-=== Igual Estricto 5 === "5" false
-
-!= Desigual normal 5 != 6 true
-!== Desigual Estricto 5 != 6 false
-
-> 
-<
-
->=
-<=
-
-Operadores logicos:
-&& and 
-
-true && true  true
-true && false  false
-false && false  false
-
-|| or
-
-true || true  true
-true || false  true
-false || false  false
-
-! not
-
-!true  false
-!false  true
-*/
