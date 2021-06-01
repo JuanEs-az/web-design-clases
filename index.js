246d25c61e2c84539289fab95eb0274bbf22fe7c
Juan Esteban Arango Zapata <juaneste687@gmail.com>
2021-06-01 18:45:33 -0500

    Ejercicio DOM p2

--- index.js
+++ index.js
@@ -12,6 +12,22 @@ const campotexto = document.getElementById("campotexto")
 const btnLimpiar = document.querySelector("#btn-limpiar")
 const btnGuardar = document.querySelector("#btn-guardar")
 const listado = document.querySelector("#listado")
+const listaNombres = []
+
+const limpiar = () => {
+    campotexto.value = ""
+    listado.innerHTML = ""
+}
+
+const actualizarListado = ( arr ) => {
+    limpiar()
+    arr.forEach(nombre => {
+        const elemento = document.createElement("li")
+        elemento.classList.add("list-group-item")
+        elemento.innerText = nombre
+        listado.appendChild( elemento )
+    });
+} 
 
 const guardar = () => {
     //Guardamos el dato
@@ -19,16 +35,12 @@ const guardar = () => {
     //Limpiamos el dato del input
     campotexto.value = ""
     //Guardarlo en la lista
-    const elemento = document.createElement("li")
-    elemento.classList.add("list-group-item")
-    elemento.innerText = valortexto
-    listado.appendChild( elemento )
+    listaNombres.push( valortexto )
+    listaNombres.sort()
+    actualizarListado( listaNombres )
 }
 btnGuardar.addEventListener("click", guardar)
-btnLimpiar.addEventListener("click", () => {
-    campotexto.value = ""
-    listado.innerHTML = ""
-})
+btnLimpiar.addEventListener("click", limpiar)
 
 const hackSpam = (nombre, cantidad) => {
     for(let i = 1; i <= cantidad; i++){
