a5b704bfc30d23fa1e0a9b69e090a8ade54c9b9e
Juan Esteban Arango Zapata <juaneste687@gmail.com>
2021-06-01 17:50:34 -0500

    Objetos y funciones

--- index.js
+++ index.js
@@ -1,75 +1,44 @@
-//Ciclos
-
-//LOS FOR
-
-//FOR BASICO
-
-for(let contador = 0; contador <= 10; contador++ ){
-    console.log( contador )
+arrays = [
+    "Juan",
+    "Colo",
+    "Simón",
+]
+obj = {
+    juan: {
+        nombre: "Juan",
+        apellido: "Arango",
+        edad: 15,
+        TI: 12345678910
+    },
+    colo: {
+        nombre: "Samuel",
+        apellido: "Colorado",
+        edad: 15,
+        TI: 10987654321
+    },
+    simon: {
+        nombre: "Simón",
+        apellido: "Posada",
+        edad: 15,
+        TI: 24681097531
+    }
 }
-//FOR IN
-let arreglo = [
-    {
-      "id": 7,
-      "email": "michael.lawson@reqres.in",
-      "first_name": "Michael",
-      "last_name": "Lawson",
-      "avatar": "https://reqres.in/img/faces/7-image.jpg"
-    },
-    {
-      "id": 8,
-      "email": "lindsay.ferguson@reqres.in",
-      "first_name": "Lindsay",
-      "last_name": "Ferguson",
-      "avatar": "https://reqres.in/img/faces/8-image.jpg"
-    },
-    {
-      "id": 9,
-      "email": "tobias.funke@reqres.in",
-      "first_name": "Tobias",
-      "last_name": "Funke",
-      "avatar": "https://reqres.in/img/faces/9-image.jpg"
-    },
-    {
-      "id": 10,
-      "email": "byron.fields@reqres.in",
-      "first_name": "Byron",
-      "last_name": "Fields",
-      "avatar": "https://reqres.in/img/faces/10-image.jpg"
-    },
-    {
-      "id": 11,
-      "email": "george.edwards@reqres.in",
-      "first_name": "George",
-      "last_name": "Edwards",
-      "avatar": "https://reqres.in/img/faces/11-image.jpg"
-    },
-    {
-      "id": 12,
-      "email": "rachel.howell@reqres.in",
-      "first_name": "Rachel",
-      "last_name": "Howell",
-      "avatar": "https://reqres.in/img/faces/12-image.jpg"
+
+let persona1 = {
+    nombre: "Jhon",
+    apellido: "Doe"
+}
+
+let persona2 = {...persona1}
+persona2.nombre = "Jane"
+console.table({ persona1, persona2 })
+
+const constructorPeronas = ( nombre, apellido ) => {
+    return {
+        nombre,
+        apellido,
+        presentarse: function(){
+            console.log(`Hola, mi nombre es ${ this.nombre } ${ this.apellido }`)
+        }
     }
-  ]
-
-  for( let indice in arreglo ){
-      console.log( arreglo[indice].first_name )
-  }
-
-  //FOR OF
-  for( let elemento of arreglo ){
-      console.log( elemento.first_name )
-  }
-
-//LOS WHILE
-
-//WHILE NORMAL
-let i = 0
-while(true){
-    console.log("SAMUEL")
-    if( i == 6 ){
-        break
-    }
-    i ++
 }
