e43aaf88c5941406e68c9c96173c42e103092a1f
Juan Esteban Arango Zapata <juaneste687@gmail.com>
2021-06-08 15:51:04 -0500

    Ejercicio Finalizado

--- index.js
+++ index.js
@@ -8,19 +8,24 @@ Cuando se presione el botón btn-limpiar:
 1. Se borra el input
 2. Se borra la lista
 */
+//Variables
 const campotexto = document.getElementById("campotexto")
 const btnLimpiar = document.querySelector("#btn-limpiar")
 const btnGuardar = document.querySelector("#btn-guardar")
 const listado = document.querySelector("#listado")
-const listaNombres = []
+const listaNombres = localStorage["listado"] ? JSON.parse(localStorage["listado"]) : []
 
+
+//Funciones
 const limpiar = () => {
     campotexto.value = ""
     listado.innerHTML = ""
+    localStorage["listado"] = '[]'
 }
 
 const actualizarListado = ( arr ) => {
-    limpiar()
+    campotexto.value = ""
+    listado.innerHTML = ""
     arr.forEach(nombre => {
         const elemento = document.createElement("li")
         elemento.classList.add("list-group-item")
@@ -32,18 +37,33 @@ const actualizarListado = ( arr ) => {
 const guardar = () => {
     //Guardamos el dato
     const valortexto = campotexto.value
+    if(valortexto == ''){
+        return 0
+    }
     //Limpiamos el dato del input
     campotexto.value = ""
     //Guardarlo en la lista
     listaNombres.push( valortexto )
     listaNombres.sort()
     actualizarListado( listaNombres )
+    localStorage["listado"] = JSON.stringify( listaNombres )
+
 }
-btnGuardar.addEventListener("click", guardar)
-btnLimpiar.addEventListener("click", limpiar)
 
 const hackSpam = (nombre, cantidad) => {
     for(let i = 1; i <= cantidad; i++){
         campotexto.value = nombre
         guardar()
     }}
+
+//Eventos
+btnGuardar.addEventListener("click", guardar)
+btnLimpiar.addEventListener("click", limpiar)
+document.addEventListener("keypress", ( event ) => {
+    if( event.keyCode === 13){
+        guardar()
+    }
+} )
+
+//Lineas imperativas
+actualizarListado( listaNombres )
